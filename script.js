e2b95cf1a757e9d3c08584c8855cd16dca215ce1
shashwatraipro21-sketch <shashwatraipro21@gmail.com>
2025-12-26 12:11:51 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Placeholder for future JavaScript features
+
+console.log("Shashwat Rai | NEET Aspirant Website Loaded");
+
+// You can later add:
+// - Progress tracker
+// - Study timer
+// - Blog section
+// - Dark/light mode
